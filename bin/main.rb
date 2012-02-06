c17ddc07c7b7b0af8cd199d1ce0e6430b0d618d7
Jiri Krizek <jkrizek@gmail.com>
2012-02-06 17:03:37 +0100

    Parsovani vstupu

--- bin/main.rb
+++ bin/main.rb
@@ -2,6 +2,22 @@
 # You are to choose the minimal amount of them, such they would completely cover 
 # the segment [0,M].
 
+class Interval
+  attr_reader :m
+
+  def initialize(interval)
+    @m = interval
+  end
+end
+
+def get_blank_line
+  blank = $stdin.readline.chomp
+
+  unless blank.empty?
+    puts "Error: Empty line expected"
+    exit
+  end
+end
 
 def get_m
     m = $stdin.readline.chomp.to_i
@@ -23,7 +39,20 @@ def get_test_cases
 end
 
 test_cases = get_test_cases
+get_blank_line
 
 for i in 1..test_cases
+  puts "Test case #{i}, enter m:"
   m = get_m
+  interval = Interval.new(m)
+  puts "Interval is [0-#{m}]"
+  
+  while(line = $stdin.readline)
+    if(line.chomp == "0 0")
+      get_blank_line
+      break
+    end
+
+    p "Line: #{line}"
+  end
 end
