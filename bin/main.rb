656e27f03012660b0e0ccd0a8b1b0a2e97928a21
Jiri Krizek <jkrizek@gmail.com>
2012-02-06 19:40:50 +0100

    Vkladani do hashmapy

--- bin/main.rb
+++ bin/main.rb
@@ -38,6 +38,25 @@ def get_m
     m
 end
 
+def get_integer(line)
+  integers=line.chomp.split(' ')
+    unless integers.size == 2
+      print "Error: interval can have only two values, you entered #{integers.size}"
+      exit
+    end
+
+    begin
+      a = Integer(integers[0])
+      b = Integer(integers[1])
+      fail ArgumentError if a>50000 || b>50000
+      interval = Interval.new(a,b)
+    rescue ArgumentError
+      print "Error: interval value is invalid"
+      exit
+    end
+    interval
+end
+
 def get_test_cases
   test_cases = $stdin.readline.chomp.to_i
 
@@ -52,6 +71,8 @@ test_cases = get_test_cases
 get_blank_line
 
 for i in 1..test_cases
+  intervals = Hash.new
+
   puts "Test case #{i}, enter m:"
   m = get_m
   dest_interval = Interval.new(m)
@@ -63,19 +84,19 @@ for i in 1..test_cases
       break
     end
     
-    integers=line.chomp.split(' ')
-    unless integers.size == 2
-      print "Error: interval can have only two values, you entered #{integers.size}"
-      exit
-    end
+    interval=get_integer(line)
+    p "New: #{interval}"
 
-    begin
-      a = Integer(integers[0])
-      b = Integer(integers[1])
-      interval = Interval.new(a,b)
-    rescue ArgumentError
-      print "Error: interval value is invalid"
-      exit
+    if intervals.key?(interval.a)
+      # Replace interval with longer interval
+      print "contains #{interval.a}"
+      intervals[interval.a] = interval if intervals[interval.a].len < interval.len 
+    else
+      print "not contain #{interval.a}"
+      intervals[interval.a] = interval
     end
+    p intervals
+    p Hash[intervals.sort]
+
   end
 end
